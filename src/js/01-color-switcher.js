98605a1f303cf05b5de281c6e6d74d79dc5e91e1
Korotkova-Iuliia <3589900@gmail.com>
2021-11-06 18:34:47 +0200

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStartEl = document.querySelector('button[data-start]');
+// window.addEventListener('keydown', onBtnStartClick);
+const btnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+let timerId = null;
+
+btnStartEl.addEventListener('click', onBtnStartClick);
+clickBtnStart.addEventListener('click', onBtnStopClick);
+
+function onBtnStartClick() {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+    btnStartEl.disabled = true;
+  }, 1000);
+}
+function onBtnStopClick {
+
+};
+// btnStartEl.removeEventListener(onBtnStartClick);
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
