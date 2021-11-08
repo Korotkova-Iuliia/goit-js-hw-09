7ca4b41ac629c50cd16b18be1a95f301c8a21b0c
Korotkova-Iuliia <3589900@gmail.com>
2021-11-08 10:51:52 +0200

    task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,6 @@ const btnStartEl = document.querySelector('button[data-start]');
 const btnStopEl = document.querySelector('button[data-stop]');
 const bodyEl = document.querySelector('body');
 const btnsEl = document.querySelectorAll('button');
-console.log(btnsEl);
-console.log(bodyEl);
 
 let timerId;
 
@@ -14,56 +12,103 @@ function onBtnStartClick(e) {
   timerId = setInterval(() => {
     bodyEl.style.backgroundColor = getRandomHexColor();
 
-    // btnStartEl.disabled = true;
-    // btnStopEl.disabled = false;
+    btnStartEl.disabled = true;
+    btnStopEl.disabled = false;
   }, 1000);
-  btnStopEl.disabled = !e.currentTarget.click;
 }
 btnStartEl.removeEventListener('click', onBtnStopClick);
 function onBtnStopClick(e) {
   clearTimeout(timerId);
-  // btnStopEl.disabled = true;
-  // btnStartEl.disabled = false;
-  btnStartEl.disabled = !e.currentTarget.click;
-  console.log(!e.currentTarget.click);
+  btnStopEl.disabled = true;
+  btnStartEl.disabled = false;
 }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 // const btnStartEl = document.querySelector('button[data-start]');
 // const btnStopEl = document.querySelector('button[data-stop]');
 // const bodyEl = document.querySelector('body');
+// const
 // const btnsEl = document.querySelectorAll('button');
 // console.log(btnsEl);
-// console.log(bodyEl);
 
-// let timerId;
+// btnsEl;
+// btnsEl.addEventListener('click', onBtnClick);
 
-// btnStartEl.addEventListener('click', onBtnStartClick);
-// btnStopEl.addEventListener('click', onBtnStopClick);
+// const options = {
+//   once: true,
 
-// function onBtnStartClick(e) {
-//   timerId = setInterval(() => {
-//     bodyEl.style.backgroundColor = getRandomHexColor();
-
-//     btnStartEl.disabled = true;
-//     btnStopEl.disabled = false;
-//   }, 1000);
-//
+//   onShow: instance => {
+//     btnsEl.addEventListener('click', onBtnClick, options);
+//   },
+//   // cloze Modal
+//   onClose: instance => {
+//     btnsEl.removeEventListener('click', onBtnClick, options);
+//   },
+// };
+// function eventHandler(e) {
+//   if (e.key === 'Escape') {
+//     instance.close();
+//     return;
+//   }
 // }
-// btnStartEl.removeEventListener('click', onBtnStopClick);
-// function onBtnStopClick(e) {
-//   clearTimeout(timerId);
-//   btnStopEl.disabled = true;
-//   btnStartEl.disabled = false;
-//
+
+// function onBtnClick(e) {
+//   console.log('один раз ');
+//   if (e.target.nodeName !== 'BUTTON') {
+//     console.log('fgesrAEs');
+//     return;
+//   }
+//   console.log('as');
+
+//   if (!e.currentTarget.click) {
+//     console.log(e.currentTarget.click);
+//   }
+//   // once: true;
+//   // console.log(!e.currentTarget.click);
+//   // // this.disableEventListener('click', onBtnStopClick);
+//   // // this.disabled = !e.currentTarget.click;
 // }
+
+// // const BtnsElChange = btnsEl.forEach(btnEl => {
+// //   btnEl.addEventListener('click', onBtnClick);
+// //   console.log('sgaergerthis:');
+// // btnEl.removeEventListener('click', onBtnStopClick);
+// // });
+
+// // console.log(btnsEl);
+// // console.log(bodyEl);
+
+// // let timerId;
+
+// // btnStartEl.addEventListener('click', onBtnStartClick);
+// // btnStopEl.addEventListener('click', onBtnStopClick);
+
+// // function onBtnStartClick(e) {
+// //   timerId = setInterval(() => {
+// //     bodyEl.style.backgroundColor = getRandomHexColor();
+
+// //     // btnStartEl.disabled = true;
+// //     // btnStopEl.disabled = false;
+// //   }, 1000);
+// //   btnStopEl.disabled = !e.currentTarget.click;
+// // }
+// // btnStartEl.removeEventListener('click', onBtnStopClick);
+// // function onBtnStopClick(e) {
+// //   clearTimeout(timerId);
+// //   // btnStopEl.disabled = true;
+// //   // btnStartEl.disabled = false;
+// //   btnStartEl.disabled = !e.currentTarget.click;
+// //   console.log(!e.currentTarget.click);
+// // }
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // }
 
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
 // const BtnsElChange = btnsEl.forEach(btnEl => {
 //   btnEl.addEventListener('click', onBtnClick);
 //   console.log('sgaergerthis:');
