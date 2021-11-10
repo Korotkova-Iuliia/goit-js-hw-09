290e536f5ceb429bd587b6b5211f39a306011b6a
Korotkova-Iuliia <3589900@gmail.com>
2021-11-10 11:41:30 +0200

    task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,61 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-// const a = 5;
-// Notify.success(`${a}`);
-// Notify.failure(
-//   'outFunction',
-//   function cb() {
-//     // callback
-//   },
-//   {
-//     timeout: 1000,
-//   },
-// );
+// // const a = 5;
+// // Notify.success(`${a}`);
+// // Notify.failure(
+// //   'outFunction',
+// //   function cb() {
+// //     // callback
+// //   },
+// //   {
+// //     timeout: 1000,
+// //   },
+// // );
+
+// const form = document.querySelector('.form');
+// console.log(form);
+// const delayEl = document.querySelector('input[name="delay"]');
+// const stepEl = document.querySelector('input[name="step"]');
+// const amountEl = document.querySelector('input[name="amount"]');
+// console.log(delayEl);
+// console.log(stepEl);
+// console.log(amountEl);
+
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(e) {
+//   e.preventDefault();
+//   // Значения полей
+//   const delay = `${delayEl.value}`;
+//   const step = `${stepEl.value}`;
+//   const amount = `${amountEl.value}`;
+//   console.log(delay);
+//   console.log(step);
+//   console.log(amount);
+
+//   const promiseCount = amount;
+
+//   for (let i = 0; i < promiseCount; i += 1) {
+//     let nextDelay = delay;
+//     nextDelay = Number(delay) + Number(step);
+//     console.log(nextDelay);
+//     createPromise(i, nextDelay);
+//   }
+//   function createPromise(position, delay) {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, function cb() {});
+//       } else {
+//         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, function cb() {});
+//       }
+//     }, delay);
+//   }
+//   createPromise();
+// }
+
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.form');
 console.log(form);
@@ -25,9 +70,7 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
-
-  e.currentTarget.elements;
-  console.log(e.currentTarget.elements);
+  // Значения полей
   const delay = `${delayEl.value}`;
   const step = `${stepEl.value}`;
   const amount = `${amountEl.value}`;
@@ -35,37 +78,45 @@ function onFormSubmit(e) {
   console.log(step);
   console.log(amount);
 
-  // Значения полей
-  const promiseCount = amount;
+  const promiseCount = Number(amount);
 
   for (let i = 0; i < promiseCount; i += 1) {
-    function createPromise(i, delay) {
-      const shouldResolve = Math.random() > 0.3;
-      if (shouldResolve) {
-        return Notify.success(
-          'Click Me',
-          function cb() {
-            // callback
-          },
-          {
-            timeout: delay,
-          },
-        );
-      } else {
-        return Notify.failure(
-          'Don`t Click Me',
-          function cb() {
-            // callback
-          },
-          {
-            timeout: delay,
-          },
-        );
-      }
-    }
-    createPromise();
+    let currentDelay = delay;
+    currentDelay = Number(currentDelay) + Number(step);
+    console.log(currentDelay);
+    createPromise(i, currentDelay);
   }
 }
+function createPromise(position, currentDelay) {
+  const promise = new Promise((resolve, reject) => {
+    const shoudResolve = Math.random > 0.3;
+
+    setTimeout(() => {
+      if (shoudResolve) {
+        return resolve({ position, currentDelay });
+      } else {
+        return reject({ position, currentDelay });
+      }
+    }, currentDelay);
+  });
+  promise
+    .then(({ position, currentDelay }) =>
+      Notify.success(`✅ Fulfilled promise ${position} in ${currentDelay}ms`),
+    )
+    .catch(({ position, currentDelay }) =>
+      Notify.failure(`❌ Rejected promise ${position} in ${currentDelay}ms`),
+    );
+}
+
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// createPromise(2, 1500)
+//    .then(({ position, delay }) => {
+//      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//    })
+//    .catch(({ position, delay }) => {
+//      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//    });
+// createPromise();
 
 // function createPromise(/* параметры */) {
 //   new Promise((resolve, reject) => {
@@ -81,45 +132,5 @@ function onFormSubmit(e) {
 //   });
 // }
 
-// const makePromise = (text, delay) => {
-//   return new Promise((resolve, reject) => {
-//  const shoudResolve = Math.random > 0.3;
-
-//     if (shoudResolve) {
-//         resolve(/* ... /);
-//       } else {
-//         reject(/ ... */);
-//       }
-//     }, delay);
-//   });
-// };
-
-// const promiseA = makePromise('promiseA value', 1000);
-// const promiseB = makePromise('promiseB value', 3000);
-
-// Promise.all([promiseA, promiseB])
-//   .then(value => console.log(value)) //["promiseA value", "promiseB value"]
-//   .catch(error => console.log(error));
-// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-// function onFormSubmit(event) {
-//   event.preventDefault();
-//   //   const formData = new FormData(event.currentTarget);
-//   //   //   console.log(formData);
-//   //   if (
-//   //     event.currentTarget.elements.email.value === '' ||
-//   //     event.currentTarget.elements.password.value === ''
-//   //   ) {
-//   //     return alert('все поля должны быть заполнены');
-//   //   }
-//   //   const userData = {};
-//   //   formData.forEach((value, name) => {
-//   //     userData[name] = value;
-//   //   });
-//   //   console.log(userData);
-//   //   form.reset();
-// }
-
 //
-
-// // \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
